1b3d6d90156eaf2aa100a0809338de4be81177c8
신진건 <sjg729729@gmail.com>
2023-08-19 00:06:09 +0900

    delete key

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,5 @@
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
-// const API_KEY = "e7af7f6dedcb0e0df4a8a9b3ba3930b4";
 const API_KEY = config.apikey;
 
 function onGeoOk(position) {
