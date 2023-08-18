4d7cf7e508a4363df279f5dcb3d9498abf7fb3a2
신진건 <sjg729729@gmail.com>
2023-08-19 00:17:30 +0900

    apikey

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,7 @@
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
-const API_KEY = config.apikey;
+// const API_KEY = config.apikey;
+const API_KEY = "e7af7f6dedcb0e0df4a8a9b3ba3930b4";
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
